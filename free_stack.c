7916247e4706be6c9376c8bbe3c77498648cf911
Debora <99737825+Debsh@users.noreply.github.com>
2022-06-17 02:11:14 +0300

    fix

--- free_stack.c
+++ free_stack.c
@@ -1,16 +1,31 @@
 #include "monty.h"
-/**
- * free_l - free list
- * @stack: head
- */
-void free_l(stack_t **stack)
-{
-	stack_t *delete = *stack;
+#include "lists.h"
 
-	while (*stack)
+/**
+ * free_all - custom memory handling function
+ * @all: flag to indicate what to free
+ */
+void free_all(int all)
+{
+	if (data.line)
 	{
-		delete = delete->next;
-		free(*stack);
-		*stack = delete;
+		free(data.line);
+		data.line = NULL;
+		free_everything(data.words);
+		data.words = NULL;
+	}
+
+	if (all)
+	{
+		if (data.stack)
+		{
+			free_dlistint(data.stack);
+			data.stack = NULL;
+		}
+		if (data.fptr)
+		{
+			fclose(data.fptr);
+			data.fptr = NULL;
+		}
 	}
 }
